550125284635feac81974be26e865ae9a624bc3f
Pragyasriv <pragyasrivastava2003bth@gmail.com>
2024-03-30 11:44:39 +0530

    initial checkin for consumer_three

---
+++ consumer_three/deletion.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import time
+import pika
+
+time.sleep(2)
+
+
+app = Flask(__name__)
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.17.0.1'))
+channel = connection.channel()
+channel.queue_declare(queue='delete_record', durable=True)
