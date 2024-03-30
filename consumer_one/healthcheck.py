e2d7d5e78fc021be19b1cb1c83eb151ed8e79848
sidhvip <117767084+sidhvip@users.noreply.github.com>
2024-03-30 11:49:27 +0530

    initial checkin for consumer_one

---
+++ consumer_one/healthcheck.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import pika
+import time
+
+time.sleep(2)
+
+app = Flask(__name__)
+
+# Set up the RabbitMQ connection
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.17.0.1'))
+channel = connection.channel()
+channel.queue_declare(queue='healthcheck', durable=True)
+
+
+# Simple Flask route for checking the health of the RabbitMQ connection
+@app.route('/healthcheck')
+def callback(ch, method, properties, body):
+    if not connection.is_closed:
+        print("Healthcheck is successful!")
+    else:
+    	print("Healthcheck unsuccessful! Connection not found!")
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+    return "Healthcheck is done!"
+
+channel.basic_qos(prefetch_count=1)
+channel.basic_consume(queue='healthcheck', on_message_callback=callback)
+channel.start_consuming()
+
