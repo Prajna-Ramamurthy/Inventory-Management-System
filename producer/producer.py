a52850de02a7e8550ad77899535a1a18de8699a2
Prajna R <prajna.ramamurthy@gmail.com>
2024-03-31 21:57:16 +0530

    Fixed RabbitMQ connection problem

--- producer/producer.py
+++ producer/producer.py
@@ -2,19 +2,19 @@ import pika
 from flask import Flask
 import time
 
-time.sleep(2)
+time.sleep(10)
 
 app = Flask(__name__)
 
 # Set up the RabbitMQ connection
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.17.0.1'))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
 
 
 # Microservice for checking the health of the RabbitMQ connection
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='172.17.0.1'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
     channel = connection.channel()
     channel.queue_declare(queue='healthcheck', durable=True)
     channel.basic_publish(
@@ -29,4 +29,4 @@ def healthcheck():
     return "Health check message sent\n"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
